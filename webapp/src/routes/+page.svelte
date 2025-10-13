a15f7f4b0096eed780a8f3616a321949134d91f8
Gabriel <gabe@gabe.blog>
2025-10-13 16:19:34 -0700

    fix: change sub-header text
    "civic engagement" -> "protests"

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -38,7 +38,7 @@
 				Protest Sourcebook
 			</h1>
 			<p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-				Document and track protests for the historical record. Help us build a comprehensive database of civic engagement.
+				Document and track protests for the historical record. Help us build a comprehensive database of protests.
 			</p>
 			<div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
 				<div class="rounded-md shadow">
